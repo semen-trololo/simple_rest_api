11436e6f36fe76075098b9b3248555750383c29d
semen-trololo <95562395+semen-trololo@users.noreply.github.com>
2022-08-10 16:45:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@
 from flask import Flask, jsonify, abort
 from flask import make_response
 from flask import request
-from flask_httpauth import HTTPBasicAuth
-auth = HTTPBasicAuth()
+import os
+
 app = Flask(__name__)
 
 tasks = [
@@ -22,55 +22,59 @@ tasks = [
     }
 ]
 
-@auth.get_password
-def get_password(username):
-    if username == 'sosed':
-        return 'python3'
-    return None
-
-
-@auth.error_handler
-def unauthorized():
-    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
-
 
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
-@auth.login_required
+@app.route('/api/v1.0/git', methods=['GET'])
+#@auth.login_required
 def get_tasks():
-    return jsonify({'tasks': tasks}) # jsonify(key1=value1 ,key2=value2 ,key3=value3)
+    dir_rep = []
+    patch = []
+    tmp = os.listdir('git\\')
+    for i in tmp:
+        if os.path.isfile(os.sep.join(['git', i])):
+            pass
+        else:
+            dir_rep.append(i)
+            patch.append(os.sep.join(['git', i]))
+    return jsonify({'repositories': dir_rep, 'patch': patch}) # jsonify(key1=value1 ,key2=value2 ,key3=value3)
 
 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
-@auth.login_required
-def get_task(task_id):
-    for id in tasks:
-        if id['id'] == task_id:
-            return jsonify({'task': id})
+@app.route('/api/v1.0/git/<string:repositories>', methods=['GET'])
+#@auth.login_required
+def get_task(repositories):
+    tmp = os.listdir('git\\')
+    for i in tmp:
+        if i == repositories:
+            tmp = os.listdir(os.sep.join(['git', i]))
+            return jsonify({'dir_list': tmp})
     abort(404)
 
 
-@app.route('/todo/api/v1.0/tasks', methods=['POST'])
-@auth.login_required
-def create_task():
-    if not request.json or not 'title' in request.json:
+@app.route('/api/v1.0/git', methods=['POST'])
+#@auth.login_required
+def create_repositories():
+    print(request.json)
+    if not request.json or not 'name' in request.json:
         abort(400)
-    task = {
-        'id': tasks[-1]['id'] + 1,
-        'title': request.json['title'],
-        'description': request.json.get('description', ""),
-        'done': False
-    }
-    tasks.append(task)
-    return jsonify({'task': task}), 201
+    os.mkdir(os.sep.join(['git', request.json['name']]))
+    dir_rep = []
+    patch = []
+    tmp = os.listdir('git\\')
+    for i in tmp:
+        if os.path.isfile(os.sep.join(['git', i])):
+            pass
+        else:
+            dir_rep.append(i)
+            patch.append(os.sep.join(['git', i]))
+    return jsonify({'repositories': dir_rep, 'patch': patch}), 201
 
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
-@auth.login_required
+#@auth.login_required
 def delete_task(task_id):
     for id in tasks:
         if id['id'] == task_id:
@@ -78,4 +82,4 @@ def delete_task(task_id):
             return jsonify({'result': True})
     abort(404)
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
