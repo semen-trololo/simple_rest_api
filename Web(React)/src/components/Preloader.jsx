c134e6bd2d472b1aea107ec437aecc946a091576
semen <semen.trololo@ya.ru>
2022-09-08 13:14:08 +0300

    Удаление старой версии

--- Web(React)/src/components/Preloader.jsx
+++
@@ -1,9 +0,0 @@
-export function Preloader() {
-	return <>
-		<div className="m-5 d-flex justify-content-center">
-			<div className="spinner-border" role="status">
-				<span className="visually-hidden">Loading...</span>
-			</div>
-		</div>
-	</>
-}
